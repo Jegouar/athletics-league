f7db91aa465b24451d9f8f18186a9ff8de551855
Jegouar <pauljegou1974@gmail.com>
2021-03-02 02:05:35 +0000

    Create session user profile template

--- app.py
+++ app.py
@@ -51,6 +51,7 @@ def register():
 
         session["user"] = request.form.get("username").lower()
         flash("Great, you are now registered!")
+        return redirect(url_for("profile", username=session["user"]))
 
     return render_template("register.html")
 
@@ -65,7 +66,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 flash("One or both of those aren't quite right")
                 return redirect(url_for("login"))
@@ -75,6 +79,13 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
