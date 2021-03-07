51c3aba4ce6b3da5bbb13bd80aabc1a603ea439d
Jegouar <pauljegou1974@gmail.com>
2021-03-07 02:49:01 +0000

    Create board styling

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def login():
     if request.method == "POST":
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-        
+
         if existing_user:
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
